44e8205fe99b4c809dd4371eab6c33bd5de0226b
Michael Sproul <micsproul@gmail.com>
2016-03-07 19:41:57 +1100

    Improved proof of progress.

--- Progress.v
+++ Progress.v
@@ -1,34 +1,65 @@
+Require Import Typing.
+Require Import Syntax.
+Require Import SmallStepSemantics.
+Require Import Empty.
+Require Export Context.
+Require Import Environments.
+
+Lemma step_app1_x : forall s e1 e2,
+  (exists s' e', step s e1 s' e') ->
+  exists s' e'', step s (TApp e1 e2) s' e''.
+Proof with eboom.
+  intros s e1 e2 Step1.
+  decompose record Step1...
+Qed.
+
+Hint Resolve step_app1_x : l3.
+
+Lemma fun_value_is_abs : forall L E e t1 t2,
+  is_empty E ->
+  L; E |- e ~: TyFun t1 t2 ->
+  value e ->
+  (exists e', e = TAbs t1 e').
+Proof.
+  intros L E e t1 t2 Empty WT Val.
+  destruct e; try (solve by inversion).
+  Case "e is a variable, impossible with an empty context".
+    inversion WT; subst; exfalso; eauto 2 with l3.
+  Case "e is a lambda abstraction, good!".
+    inversion WT; subst.
+    eauto.
+Qed.
+
+Hint Resolve fun_value_is_abs : l3.
+
+Lemma value_app1_x : forall L E1 s e1 e2 t1 t2,
+  is_empty E1 ->
+  L; E1 |- e1 ~: TyFun t1 t2 ->
+  value e1 ->
+  (exists s' e2', step s e2 s' e2') \/ value e2 ->
+  (exists s' e'', step s (TApp e1 e2) s' e'').
+Proof with eboom.
+  intros L E1 s e1 e2 t1 t2 Empty1 WT1 Val1 [Step2 | Val2].
+  Case "e2 steps".
+    decompose record Step2...
+  Case "e2 is also a value".
+    (* Beta reduction *)
+    assert (exists e1', e1 = TAbs t1 e1') as [e1']...
+    subst e1...
+Qed.
+
+Hint Resolve value_app1_x : l3.
+
 (* Proof of progress! *)
 Theorem progress : forall L E e s t,
   is_empty L ->
   is_empty E ->
   L; E |- e ~: t ->
   (exists s' e', step s e s' e') \/ value e.
-Proof with eauto.
+Proof with eboom.
   intros L E e s t EmptyL EmptyE WT.
-  induction WT; auto.
+  induction WT...
   Case "Application".
-  (* First, reasoning about the context splitting. We want E1 and E2 empty. *)
-  assert (is_empty E1 /\ is_empty E2) as [EmptyE1 EmptyE2].
-    eauto using empty_context.
-  destruct IHWT1 as [Step_e1 | Value_e1]...
-  (* e1 steps *)
-  SCase "e1 steps".
-    (* Proof by StepApp1 (invert stepping of e1) *)
-    inversion Step_e1. inversion H... (* FIXME: naming here *)
-  SCase "e1 is a value".
-    destruct IHWT2 as [Step_e2 | Value_e2]...
-    SSCase "e2 steps".
-      inversion Step_e2... inversion H... (* FIXME: same naming fix needed here *)
-    SSCase "e2 is a value".
-      (* Here we use beta reduction, by first showing that e1 must be a lambda expression *)
-      left.
-      destruct e1; try (solve by inversion).
-      SSSCase "e1 is a TVar".
-        (* Var case is impossible *)
-        inversion WT1; subst.
-        exfalso. eauto using insert_empty_contra.
-      (* Beta reduction! *)
-      exists s.
-      exists (subst e2 0 e1)...
+    assert (is_empty E1 /\ is_empty E2) as [? ?]...
+    destruct IHWT1...
 Qed.
