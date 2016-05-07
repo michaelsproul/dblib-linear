0424bf85822d17a6209a32d9661129789fa57d1d
Michael Sproul <micsproul@gmail.com>
2016-05-07 21:55:11 +1000

    Prove the soundness corollary.

---
+++ Soundness.v
@@ -0,0 +1,30 @@
+Require Import Syntax.
+Require Import Typing.
+Require Import Empty.
+Require Import SmallStepSemantics.
+Require Import Progress.
+Require Import Preservation.
+
+Inductive multi_step : term -> term -> Prop :=
+  | multi_step_refl e : multi_step e e
+  | multi_step_trans e e' e''
+      (SingleStep : step e e')
+      (MultiStep : multi_step e' e'') :
+      multi_step e e''.
+
+Hint Constructors multi_step : l3.
+
+Corollary soundness : forall E e e' t,
+  is_empty E ->
+  E |- e ~: t ->
+  multi_step e e' ->
+  (exists e'', step e' e'') \/ value e'.
+Proof with eboom.
+  intros E e e' t Empty WT Steps.
+  induction Steps.
+  Case "zero steps".
+    eapply progress...
+  Case "one or more steps".
+    apply IHSteps.
+    eapply preservation...
+Qed.
